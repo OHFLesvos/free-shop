43ee92c96bc8bd324517453e22f06e041a50526c
Nicolas Perrenoud <mail@nicu.ch>
2020-12-12 00:36:31 +0200

    Added HTTPS middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \anlutro\LaravelSettings\SaveMiddleware::class,
+        \App\Http\Middleware\HttpsProtocol::class,
     ];
 
     /**
