b80034a25f1a46c16ebbab0886afb3c120d4b015
Nicolas Perrenoud <mail@nicu.ch>
2020-12-11 19:32:10 +0200

    Translation of SMS message

--- app/Listeners/SendOrderNotificationToCustomer.php
+++ app/Listeners/SendOrderNotificationToCustomer.php
@@ -29,12 +29,12 @@ class SendOrderNotificationToCustomer
     {
         if ($this->smsSender->isConfigured())
         {
-            $message = sprintf("Hello %s (%s), we received your order with ID #%d and will get back to you soon.\nYou ordered: %s",
-                $event->order->customer_name,
-                $event->order->customer_id_number,
-                $event->order->id,
-                $event->order->products->map(fn ($product) => $product->pivot->amount . 'x ' . $product->name)->join(', ')
-            );
+            $message = __("Hello :customer_name (:customer_id), we received your order with ID #:id and will get back to you soon.\nYou ordered the following items: :items", [
+                'customer_name' => $event->order->customer_name,
+                'customer_id' => $event->order->customer_id_number,
+                'id' => $event->order->id,
+                'items' => $event->order->products->map(fn ($product) => $product->pivot->amount . 'x ' . $product->name)->join(', ')
+            ]);
             $this->smsSender->sendMessage($message, $event->order->customer_phone);
         } else {
             Log::error('Unable to send SMS to customer; SMS service is not configured.', [
