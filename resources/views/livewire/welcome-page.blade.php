d94af387eafaa0bb8e571e586b533bd46d549a1c
Nicolas Perrenoud <mail@nicu.ch>
2021-02-06 15:01:51 +0200

    Revert "test edit"
    This reverts commit 81617a88f2031aa2198a8a77755afdea94e17aee.

--- resources/views/livewire/welcome-page.blade.php
+++ resources/views/livewire/welcome-page.blade.php
@@ -5,5 +5,5 @@
         <h1>Welcome to {{ config('app.name') }}</h1>
         <p>Click the button below to see the available products.</p>
     @endisset
-    <p><a href="{{ route('shop-front') }}" class="btn btn-primary">@lang('Go to the nice shop')</a></p>
+    <p><a href="{{ route('shop-front') }}" class="btn btn-primary">@lang('Go to the shop')</a></p>
 </div>
