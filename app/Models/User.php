99ccaacc153799b230c8fe11b430fa3aab2050d5
Nicolas Perrenoud <mail@nicu.ch>
2021-02-13 17:45:34 +0200

    Added permission package

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
     use NullableFields;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
