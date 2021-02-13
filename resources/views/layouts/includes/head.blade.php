1184241b2bbe93ac03d72174930013a498926741
Nicolas Perrenoud <mail@nicu.ch>
2021-02-13 17:13:48 +0200

    Added favicon path

--- resources/views/layouts/includes/head.blade.php
+++ resources/views/layouts/includes/head.blade.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@isset($title){{ $title }} | @endisset{{ config('app.name') }}</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('favicon.ico') }}" rel="icon">
     @livewireStyles
     @stack('styles')
 </head>
