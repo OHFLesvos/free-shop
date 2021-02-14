40e69db478d39f30bb620d0cd373be4198ef56bd
Nicolas Perrenoud <mail@nicu.ch>
2021-02-14 17:19:02 +0200

    Permission warning on dashboard

--- resources/views/livewire/backend/dashboard-page.blade.php
+++ resources/views/livewire/backend/dashboard-page.blade.php
@@ -2,8 +2,13 @@
     @if(setting()->has('shop.disabled', false))
         <x-alert type="warning">The shop is currently disabled.</x-alert>
     @endif
+    @if(Auth::user()->roles->isEmpty() && Auth::user()->permissions->isEmpty())
+        <x-alert type="warning">
+            You don't seem to have any permissions yet.<br>
+            Please ask an administrator to assign you one or more roles.
+        </x-alert>
+    @endif
     <div class="row row-cols-1 row-cols-md-2 gx-4 gy-2">
-
         @can('viewAny', App\Models\Order::class)
             <div class="col">
                 <x-card>
