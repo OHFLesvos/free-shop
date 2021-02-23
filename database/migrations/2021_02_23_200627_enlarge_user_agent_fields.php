b55f705ac962941c73d1c46e8f21e3e3b50a711b
Nicolas Perrenoud <mail@nicu.ch>
2021-02-23 22:16:18 +0200

    Fix length of user agent fields

---
+++ database/migrations/2021_02_23_200627_enlarge_user_agent_fields.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EnlargeUserAgentFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('last_login_user_agent')->change();
+        });
+        Schema::table('orders', function (Blueprint $table) {
+            $table->text('user_agent')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('user_agent')->change();
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('last_login_user_agent')->change();
+        });
+    }
+}
