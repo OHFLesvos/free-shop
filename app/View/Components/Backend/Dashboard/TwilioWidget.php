2396f29d0d46e35a6615f3598eab573543ac0d37
Nicolas Perrenoud <mail@nicu.ch>
2021-03-03 22:34:40 +0200

    Cleanup

--- app/View/Components/Backend/Dashboard/TwilioWidget.php
+++ app/View/Components/Backend/Dashboard/TwilioWidget.php
@@ -8,16 +8,6 @@ use Illuminate\View\Component;
 class TwilioWidget extends Component
 {
     /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Get the view / contents that represent the component.
      *
      * @return \Illuminate\Contracts\View\View|string
--- app/View/Components/Backend/Dashboard/UsersWidget.php
+++ app/View/Components/Backend/Dashboard/UsersWidget.php
@@ -9,23 +9,13 @@ use Illuminate\View\Component;
 class UsersWidget extends Component
 {
     /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Get the view / contents that represent the component.
      *
      * @return \Illuminate\Contracts\View\View|string
      */
     public function render()
     {
-        if (Auth::user()->can('viewAny', App\Models\User::class)) {
+        if (Auth::user()->can('viewAny', User::class)) {
             return view('components.backend.dashboard.users-widget', [
                 'registeredUsers' => User::count(),
             ]);
