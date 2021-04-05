b7341374586004ff78d4f050a22a1e70b1f61d91
Nicolas Perrenoud <nicolas@ohf-lesvos.org>
2021-04-05 10:01:44 +0300

    simplified welcome page

--- resources/views/livewire/welcome-page.blade.php
+++ resources/views/livewire/welcome-page.blade.php
@@ -1,9 +1,11 @@
+@inject('textRepo', 'App\Repository\TextBlockRepository')
+
 <div class="small-container">
-    @isset($content)
-        {!! $content !!}
+    @if($textRepo->exists('welcome'))
+        {!! $textRepo->getMarkdown('welcome') !!}
     @else
         <h1>Welcome to {{ config('app.name') }}</h1>
         <p>Click the button below to see the available products.</p>
-    @endisset
+    @endif
     <p><a href="{{ route('shop-front') }}" class="btn btn-primary">@lang('Go to the shop')</a></p>
 </div>
