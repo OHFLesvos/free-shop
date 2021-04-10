d2a920d80a015fd55530cf56610b0af1326b654e
Nicolas Perrenoud <mail@nicu.ch>
2021-04-10 17:29:43 +0300

    Copyright in language selector page

--- resources/views/language-select.blade.php
+++ resources/views/language-select.blade.php
@@ -14,4 +14,10 @@
             @endforeach
         </div>
     </div>
+    <p class="text-center">
+        <small>
+            &copy;{{ now()->format('Y') }}
+            <a href="https://ohf-lesvos.org" target="_blank">OHF Lesvos</a>
+        </small>
+    </p>
 @endsection
