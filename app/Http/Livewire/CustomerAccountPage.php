74bfe158bc8d853d854d1d94164b9e1d55b0eb02
Nicolas Perrenoud <nicolas@ohf-lesvos.org>
2021-08-23 14:40:46 +0300

    Added phpstan

--- app/Http/Livewire/CustomerAccountPage.php
+++ app/Http/Livewire/CustomerAccountPage.php
@@ -90,7 +90,7 @@ class CustomerAccountPage extends FrontendPage
             'event.kind' => 'event',
             'event.outcome' => 'success',
             'customer.name' => $this->customer->name,
-            'customer.id_number' => $this->customer->idNumber,
+            'customer.id_number' => $this->customer->id_number,
             'customer.phone' => $this->customer->phone,
             'customer.email' => $this->customer->email,
         ]);
