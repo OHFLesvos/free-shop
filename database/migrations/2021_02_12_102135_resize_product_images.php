cb6ca899f6e17b769247050ecd8895332d1cc1be
Nicolas Perrenoud <mail@nicu.ch>
2022-02-10 20:19:53 +0200

    format code

--- database/migrations/2021_02_12_102135_resize_product_images.php
+++ database/migrations/2021_02_12_102135_resize_product_images.php
@@ -18,12 +18,12 @@ return new class extends Migration
             ->filter(fn ($product) => !preg_match('#^http[s]?://#', $product->picture))
             ->map(fn ($product) => Storage::path($product->picture))
             ->filter(fn ($path) => is_file($path))
-            ->each(function($path) {
+            ->each(function ($path) {
                 $image = new ImageResize($path);
                 $image->resizeToWidth(config('shop.product.max_picture_width'));
                 $image->save($path);
             });
-        }
+    }
 
     /**
      * Reverse the migrations.
--- database/migrations/2021_05_16_220444_create_tags_table.php
+++ database/migrations/2021_05_16_220444_create_tags_table.php
@@ -25,7 +25,6 @@ return new class extends Migration
             $table->unsignedBigInteger('taggable_id');
             $table->string('taggable_type');
         });
-
     }
 
     /**
