c4b956b6a61b693806d3da927e96808964e8a322
Nicolas Perrenoud <mail@nicu.ch>
2022-05-04 19:10:24 +0300

    Order reg frmat

--- resources/views/livewire/backend/order-register-page.blade.php
+++ resources/views/livewire/backend/order-register-page.blade.php
@@ -10,54 +10,68 @@
         @endif
 
         <x-card title="Register order for {{ $customer->name }}">
-            <div class="pt-3">
-            @foreach ($products->groupBy('category') as $k => $v)
-                <h6 class="card-subtitle mb-2">{{ $k }}</h6>
-                @foreach ($v as $product)
-                <div class="row mb-2 align-items-center g-2">
-                    <div class="col-sm">
-                        <label for="product{{ $product->id }}Input">
-                            {{ $product->name }}
-                        </label>
-                    </div>
-                    <div class="col-sm-2">
-                        @if($product->price > 0 && $product->currency_id !== null)
-                            <strong>{{ $product->price }}</strong> {{ $product->currency->name }}
-                        @else
-                            <strong class="text-success">Free</strong>
-                        @endif
-                    </div>
-                    <div class="col-sm-2">
-                        Limit: <strong>{{ $product->getAvailableQuantityPerOrder() }}</strong>
-                    </div>
-                    <div class="col-sm-2">
-                        <input
-                            type="number"
-                            min="0"
-                            max="{{ $product->getAvailableQuantityPerOrder() }}"
-                            wire:model="selection.{{ $product->id }}"
-                            placeholder="0"
-                            class="form-control"
-                            id="product{{ $product->id }}Input">
+            <x-slot name="addon">
+                <ul class="list-group list-group-flush">
+                    @foreach ($products->groupBy('category') as $k => $v)
+                        <li class="list-group-item">
+                            <h6 class="my-2">{{ $k }}</h6>
+                        </li>
+                        @foreach ($v as $product)
+                            <li class="list-group-item">
+                                <div class="row align-items-center g-2">
+                                    <div class="col-sm">
+                                        <label for="product{{ $product->id }}Input">
+                                            {{ $product->name }}
+                                        </label>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        @if($product->price > 0 && $product->currency_id !== null)
+                                            <strong>{{ $product->price }}</strong> {{ $product->currency->name }}
+                                        @else
+                                            <strong class="text-success">Free</strong>
+                                        @endif
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <small>
+                                            Available: {{ $product->freeQuantity }}<br>
+                                            Limit: {{ $product->getAvailableQuantityPerOrder() }}
+                                        </small>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <input
+                                            type="number"
+                                            min="0"
+                                            max="{{ $product->getAvailableQuantityPerOrder() }}"
+                                            wire:model="selection.{{ $product->id }}"
+                                            placeholder="0"
+                                            class="form-control"
+                                            id="product{{ $product->id }}Input">
+                                    </div>
+                                </div>
+                            </li>
+                        @endforeach
+                    @endforeach
+                    <li class="list-group-item text-end">
+                        <strong>Total costs:</strong>
+                        {{ $this->totalPrice }}
+                        <br>
+                        <strong>Customer balance:</strong>
+                        {{ $customer->totalBalance() }}
+                    </li>
+                </ul>
+                <div class="card-body">
+                    <div>
+                        <label for="remarksInput" class="form-label">Remarks</label>
+                        <textarea
+                            class="form-control @error('order.remarks') is-invalid @enderror"
+                            id="remarksInput"
+                            autocomplete="off"
+                            rows="3"
+                            wire:model.defer="order.remarks"></textarea>
+                        @error('order.remarks') <div class="invalid-feedback">{{ $message }}</div> @enderror
                     </div>
                 </div>
-            @endforeach
-            <hr>
-            @endforeach
-        </div>
-
-        <p class="text-end">
-            <strong>Total costs:</strong>
-            {{ $this->totalPrice }}
-        </p>
-
-            <div>
-                <label for="remarksInput" class="form-label">Remarks</label>
-                <textarea class="form-control @error('order.remarks') is-invalid @enderror" id="remarksInput"
-                    autocomplete="off" rows="3" wire:model.defer="order.remarks"></textarea>
-                @error('order.remarks') <div class="invalid-feedback">{{ $message }}</div> @enderror
-            </div>
-
+            </x-slot>
             <x-slot name="footer">
                 <div class="d-flex justify-content-between">
                     @can('view', $customer)
