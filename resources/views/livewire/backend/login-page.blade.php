2dede130169aba6645ea1db64ce909ff4fae425f
Nicolas Perrenoud <mail@nicu.ch>
2022-08-13 19:21:18 +0300

    Handle case when there are no local users and no oauth provider

--- resources/views/livewire/backend/login-page.blade.php
+++ resources/views/livewire/backend/login-page.blade.php
@@ -2,65 +2,69 @@
     <h1 class="display-4 text-center">{{ config('app.name') }}</h1>
     <h2 class="mb-4 display-6 text-center">Backend Login</h2>
 
-    <div class="row g-4 mb-4">
-        @if($hasLocalLogin)
-            <div class="col-md">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h5 class="card-title mb-3 text-center">Login with local account</h5>
-                        <form wire:submit.prevent="submit" autocomplete="off">
-                            <div class="mb-3">
-                                <input
-                                    type="email"
-                                    class="form-control @error('email') is-invalid @enderror"
-                                    placeholder="Email address"
-                                    required
-                                    autofocus
-                                    autocomplete="off"
-                                    wire:loading.attr="disabled"
-                                    wire:model.defer="email">
-                                @error('email') <div class="invalid-feedback">{{ $message }}</div> @enderror
-                            </div>
-                            <div class="mb-3">
-                                <input
-                                    type="password"
-                                    class="form-control @error('password') is-invalid @enderror"
-                                    placeholder="Password"
-                                    required
-                                    autocomplete="off"
-                                    wire:loading.attr="disabled"
-                                    wire:model.defer="password">
-                                @error('password') <div class="invalid-feedback">{{ $message }}</div> @enderror
-                            </div>
-                            <div class="d-grid">
-                                <button type="submit" class="btn btn-primary">Login</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        @endif
-        @if(count($oauth) > 0)
-            <div class="col-md">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h5 class="card-title mb-3 text-center">Login with service provider</h5>
-                        <div class="row row-cols-1 g-3">
-                            @foreach ($oauth as $service)
-                                <div class="col d-grid">
-                                    <a href="{{ $service['url'] }}" class="btn btn-outline-primary">
-                                        <x-icon type="brands" :icon="$service['icon']"/>
-                                        {{ $service['label'] }}
-                                        @if($service['domain'] != null)({{ $service['domain'] }})@endif
-                                    </a>
+    @if(!$hasLocalLogin && count($oauth) == 0)
+        <x-alert type="warning">No OAuth provider configured.</x-alert>
+    @else
+        <div class="row g-4 mb-4">
+            @if($hasLocalLogin)
+                <div class="col-md">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h5 class="card-title mb-3 text-center">Login with local account</h5>
+                            <form wire:submit.prevent="submit" autocomplete="off">
+                                <div class="mb-3">
+                                    <input
+                                        type="email"
+                                        class="form-control @error('email') is-invalid @enderror"
+                                        placeholder="Email address"
+                                        required
+                                        autofocus
+                                        autocomplete="off"
+                                        wire:loading.attr="disabled"
+                                        wire:model.defer="email">
+                                    @error('email') <div class="invalid-feedback">{{ $message }}</div> @enderror
                                 </div>
-                            @endforeach
+                                <div class="mb-3">
+                                    <input
+                                        type="password"
+                                        class="form-control @error('password') is-invalid @enderror"
+                                        placeholder="Password"
+                                        required
+                                        autocomplete="off"
+                                        wire:loading.attr="disabled"
+                                        wire:model.defer="password">
+                                    @error('password') <div class="invalid-feedback">{{ $message }}</div> @enderror
+                                </div>
+                                <div class="d-grid">
+                                    <button type="submit" class="btn btn-primary">Login</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-            </div>
-        @endif
-    </div>
+            @endif
+            @if(count($oauth) > 0)
+                <div class="col-md">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h5 class="card-title mb-3 text-center">Login with service provider</h5>
+                            <div class="row row-cols-1 g-3">
+                                @foreach ($oauth as $service)
+                                    <div class="col d-grid">
+                                        <a href="{{ $service['url'] }}" class="btn btn-outline-primary">
+                                            <x-icon type="brands" :icon="$service['icon']"/>
+                                            {{ $service['label'] }}
+                                            @if($service['domain'] != null)({{ $service['domain'] }})@endif
+                                        </a>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+        </div>
+    @endif
 
     <p class="text-center">
         <a href="{{ route('home') }}">Return to shop</a>
