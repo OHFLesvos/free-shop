a5d814117e05e7126e46f65b8a5c776c93efd0d6
Qiong Wu <qiong.wu@gfnork.de>
2023-08-22 13:57:37 +0300

    Update head.blade.php

--- resources/views/layouts/includes/head.blade.php
+++ resources/views/layouts/includes/head.blade.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@isset($title){{ $title }} | @endisset{{ setting()->get('brand.name', config('app.name')) }}</title>
     @vite(['resources/js/app.js', 'resources/sass/app.scss'])
-    @if(setting()->has('brand.favicon') && Storage::exists(setting()->get('brand.favicon')))
+    @if(setting()->has('brand.favicon'))
         <link href="{{ storage_url(setting()->get('brand.favicon')) }}" rel="icon">
     @else
         <link href="{{ asset('favicon.ico') }}" rel="icon">
